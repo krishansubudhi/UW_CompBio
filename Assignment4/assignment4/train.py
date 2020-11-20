17b45995b687929e48bd99825b87fd60248fd815
Krishan Subudhi <krkusuk@microsoft.com>
2020-11-20 10:31:29 -0800

    trainer test cases complete

--- Assignment4/assignment4/train.py
+++ Assignment4/assignment4/train.py
@@ -1,5 +1,6 @@
 from .viterbi import ViterbiInference
 import pandas as pd
+import numpy as np
 import collections
 class ViterbiTrain:
     def __init__(self, sequence , emmisionP : pd.DataFrame, transitionP : pd.DataFrame, begin_state = 'B' ):
@@ -8,31 +9,49 @@ class ViterbiTrain:
         self.begin_state = begin_state
         self.states = [key for key in transitionP.keys() if key !=self.begin_state]
         self.sequence = sequence
+        self.pseudo_count = 0 # change to 0.00001 if log(0) is encountered 
     
     def expectation(self):
-        viterbi = ViterbiInference(self.emmisionP, self.transitionP, self.begin_state)
-        v_all = viterbi.get_viterbi_path(self.sequence)
-        path = viterbi.traceback(v_all)
+        self.viterbi = ViterbiInference(self.emmisionP, self.transitionP, self.begin_state)
+        v_all = self.viterbi.get_viterbi_path(self.sequence)
+        path = self.viterbi.traceback(v_all)
         return path
 
-    def maximization(self, path, hits):
+    def maximization(self, path):
         #Emission
         ef, tf = self.calculateFrequencies(path)
+        ef += self.pseudo_count
+        tf += self.pseudo_count
+        self.updateTransitionP(tf)
+        self.updateEmissionP(ef)
+
 
     def em_step(self):
         path = self.expectation()
         maximization(path)
     
+    def updateTransitionP(self, tf):
+        #calculate probability
+        p = tf.multiply(1/tf.sum(axis = 1), axis = 0)
+        #Use original begin state
+        p.loc[self.begin_state] = self.transitionP.loc[self.begin_state]
+        self.transitionP = p
+    
+    def updateEmissionP(self, ef):
+        p = ef.multiply(1/ef.sum(axis = 1), axis = 0)
+        self.emmisionP = p
+
     def calculateFrequencies(self, path):
-        emission = {state: collections.defaultdict(int) for state in self.states}
-        transition = {state: collections.defaultdict(int) for state in self.states}
+        emission = (self.emmisionP * 0).astype(int) #{state: collections.defaultdict(int) for state in self.states}
+        transition = (self.transitionP * 0).astype(int) #{state: collections.defaultdict(int) for state in self.states}
         assert len(self.sequence) == len(path)
         prev_state = None
         for nt, state in zip(self.sequence, path):
-            emission[state][nt] += 1
+            emission.loc[state][nt] += 1
             if prev_state:
-                transition[prev_state][state] += 1
+                transition.loc[prev_state][state] += 1
+            prev_state = state
 
-        return pd.DataFrame(emission).T, pd.DataFrame(transition).T
+        return emission, transition
         # return df.multiply(1/df.sum(axis = 1), axis = 0)
     
---
+++ Assignment4/test/test_train.py
@@ -0,0 +1,82 @@
+from unittest import TestCase
+from unittest.mock import MagicMock
+from assignment4.train import *
+
+class TestTrain(TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        print('Init called') #called before every function
+    
+    @classmethod
+    def setUpClass(self):
+        #called once at the beginning
+        self.emmisionP = pd.DataFrame([ [1/6]*6, 
+                                  [1/10]*5+[1/2] ], 
+                                  index = ['F','L'], columns = range(1,7))
+        self.transitionP = pd.DataFrame([[0.52,0.48],
+                                    [0.6,0.4], 
+                                    [0.17, 0.83]], 
+                                    columns = ['L','F'], index = ['B','L','F'])
+        self.sequence = [3,1,6,6,6,4]
+        self.trainer = ViterbiTrain(self.sequence, self.emmisionP, self.transitionP, 'B')
+        print('setUpClass called')
+
+    def setUp(self):
+        print('Set up called') # called before every function
+    
+    def test_sanity(self):
+        assert (self.trainer.emmisionP == self.emmisionP).all().all()
+
+    def test_expectation(self):
+        path = self.trainer.expectation()
+        assert path == ['F', 'F', 'L', 'L', 'L', 'F']
+
+    def test_calculateFrequencies(self):
+        path = ['F', 'F', 'L', 'L', 'L', 'F']
+        #self.sequence = [3,1,6,6,6,4]
+        ef, tf = self.trainer.calculateFrequencies(path)
+        print(ef)
+        print(tf)
+        assert ef.loc['F'][6] == 0
+        assert tf.loc['F']['L'] == 1
+    
+    def test_updateTransitionP(self):
+        tf = pd.DataFrame([ [0,0],
+                            [2,1],
+                            [1,1]
+                        ], index = list('BLF'), columns = list('LF'))
+        self.trainer.updateTransitionP(tf)
+        print(self.trainer.transitionP)
+        truth = np.array(self.trainer.transitionP.values == [
+                                                [0.52, 0.48 ],
+                                                [2/3,1/3],
+                                                [0.5,0.5]
+                                            ])
+        # print(truth, truth.all())
+        assert truth.all()
+
+    def test_updateEmissionP(self):
+        ef = pd.DataFrame([ [1]*6,
+                            [1]*5+[2]
+                        ], index = ['F','L'], columns = range(1,7))
+        self.trainer.updateEmissionP(ef)
+        print(self.trainer.emmisionP)
+        # truth = np.array(self.trainer.transitionP.values == [
+        #                                         [1/6]*6,
+        #                                         [1/7]*5+[2/7]
+        #                                     ])
+        # print(truth, truth.all()) #rounding error
+        assert self.trainer.emmisionP.loc['F'].sum().round() == 1
+        assert self.trainer.emmisionP.loc['L'].sum().round() == 1
+        assert (self.trainer.emmisionP.loc['L'][6]/self.trainer.emmisionP.loc['F'][6]).round() == 2
+
+    def test_maximization(self):
+        path = ['F', 'F', 'L', 'L', 'L', 'F']
+        #self.sequence = [3,1,6,6,6,4]
+        self.trainer.maximization(path)
+        print(self.trainer.emmisionP)
+        print(self.trainer.transitionP)
+        
+        assert self.trainer.emmisionP.loc['L'][6] == 1
+        assert self.trainer.transitionP.loc['L']['F'] == 1/3
