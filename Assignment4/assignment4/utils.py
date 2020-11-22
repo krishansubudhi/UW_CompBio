467e19048d12e8f2c9b877f58063f8072dd8f762
Krishan Subudhi <krkusuk@microsoft.com>
2020-11-21 18:41:07 -0800

    Completed assignment- report pending

--- Assignment4/assignment4/utils.py
+++ Assignment4/assignment4/utils.py
@@ -11,3 +11,15 @@ def get_seqs_from_file(file_path:str):
             line = ''.join([c if c in list('ACGT') else 'T' for c in line])
             sequences[-1] +=line
     return sequences
+
+import re
+def get_genes_from_file(file_path:str):
+    # assert filerpath.endswith('.fasta')
+    genepos = []
+    with open(file_path,'r') as file:
+        lines = file.readlines()
+    for line in lines:
+        if re.search('biotype=.*RNA',line):
+            line = line.split()
+            genepos.append((int(line[3]),int(line[4])))
+    return genepos
--- Assignment4/test/test_utils.py
+++ Assignment4/test/test_utils.py
@@ -16,3 +16,17 @@ def test_get_seqs_from_file():
     assert len(sequences) == 2
     assert sequences[0] == 'ACGTTTATGGC'
     assert sequences[1] == 'ACTG'
+
+def test_get_genes_from_file():
+    content ='''
+    NC_000909.1  RefSeq       gene  97426  97537  .  -  .   ID=gene102;Dbxref=GeneID:1450942;Name=MJ_RS00515;gbkey=Gene;gene_biotype=tRNA;locus_tag=MJ_RS00515;old_locus_tag=MJ_t01%2CMJt01%2CtRNA-Met-1
+    NC_000909.1  tRNAscan-SE  tRNA  97426  97537  .  -  .   ID=rna0;Parent=gene102;Dbxref=GeneID:1450942;anticodon=(pos:complement(97501..97503));gbkey=tRNA;inference=COORDINATES: profile:tRNAscan-SE:1.23;product=tRNA-Met
+    NC_000909.1  tRNAscan-SE  exon  97500  97537  .  -  .   ID=id2;Parent=rna0;Dbxref=GeneID:1450942;anticodon=(pos:complement(97501..97503));gbkey=tRNA;inference=COORDINATES: profile:tRNAscan-SE:1.23;product=tRNA-Met
+    NC_000909.1  tRNAscan-SE  exon  97426  97464  .  -  .   ID=id3;Parent=rna0;Dbxref=GeneID:1450942;anticodon=(pos:complement(97501..97503));gbkey=tRNA;inference=COORDINATES: profile:tRNAscan-SE:1.23;product=tRNA-Met
+    '''
+    filepath = os.path.join(tempfile.gettempdir(), 'genes.gff')
+    with open(filepath, 'w') as f:
+        f.write(content)
+    genepos = get_genes_from_file(filepath)
+    print(genepos)
+    assert genepos[0] == (97426, 97537)
